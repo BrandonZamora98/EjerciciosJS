4c0308d01f54bbda5b0aa34c951ac059011b378a
BrandonZamora98 <brandonalvazamora@gmail.com>
2022-08-02 18:20:13 -0500

    Ejercicio de corregir malas practicas de JS

---
+++ assets/script/badScript.js
@@ -0,0 +1,35 @@
+//  Bad practices
+var segundo = 1;
+var second = 1;
+
+// Why doesn't it work?
+var ifCondition =  null;
+
+
+// Should equal formula for wild wings
+var Buffalowings = wings + buffalo
+
+
+// Null variable
+var Null = Null
+
+
+// All variables should equal 1
+var uno = one = wan = uma = um = 1;
+
+
+// It's broken
+console.log(.1);
+
+
+// Console.log para mostrar [area] de un cuadrado
+var ladoa = 4;
+var ladob = 2;
+const multiplicacion = ladoa * ladob;
+console.log(multiplicacion);
+
+// Should equal 4
+var four = 2 + 2;
+
+// Doesn't work for math problems
+const pi = 3.141516;
